49aad4652d2a45eb41c04c5f60551480bdaf9edc
Kien Nguyen <kien.n.quang@gmail.com>
2024-08-31 01:56:49 -0700

    w32 emacs-abort: remove kbdhook

--- src/w32fns.c
+++ src/w32fns.c
@@ -2883,6 +2883,10 @@ setup_w32_kbdhook (HWND hwnd)
 void
 remove_w32_kbdhook (void)
 {
+  /* Avoid over release */
+  if (kbdhook.hook_count == 0)
+    return;
+
   kbdhook.hook_count--;
   if (kbdhook.hook_count == 0 && w32_kbdhook_active)
     {
@@ -11343,6 +11347,9 @@ emacs_abort (void)
 {
   if (w32_disable_abort_dialog)
     abort ();
+
+  /* On the last leg already, release kbdhook */
+  remove_w32_kbdhook();
 
   int button;
 
