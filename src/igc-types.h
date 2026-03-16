d15a0c9c5181feefb8d6294c62de67b19e2614d4
Kien Nguyen <kien.n.quang@gmail.com>
2026-03-16 11:40:15 -0700

    Fix IGC header bitfield layout on MinGW

--- src/igc-types.h
+++ src/igc-types.h
@@ -94,19 +94,19 @@ union igc_header
 #ifndef WORDS_BIGENDIAN
   struct
   {
-    enum igc_tag tag : IGC_HEADER_TAG_BITS;
-    enum igc_obj_type obj_type : IGC_HEADER_TYPE_BITS;
-    size_t hash : IGC_HEADER_HASH_BITS;
-    size_t nwords : IGC_HEADER_NWORDS_BITS;
+    uint64_t tag : IGC_HEADER_TAG_BITS;
+    uint64_t obj_type : IGC_HEADER_TYPE_BITS;
+    uint64_t hash : IGC_HEADER_HASH_BITS;
+    uint64_t nwords : IGC_HEADER_NWORDS_BITS;
   } s;
 #endif
 #ifdef WORDS_BIGENDIAN
   struct
   {
-    size_t nwords : IGC_HEADER_NWORDS_BITS;
-    size_t hash : IGC_HEADER_HASH_BITS;
-    enum igc_obj_type obj_type : IGC_HEADER_TYPE_BITS;
-    enum igc_tag tag : IGC_HEADER_TAG_BITS;
+    uint64_t nwords : IGC_HEADER_NWORDS_BITS;
+    uint64_t hash : IGC_HEADER_HASH_BITS;
+    uint64_t obj_type : IGC_HEADER_TYPE_BITS;
+    uint64_t tag : IGC_HEADER_TAG_BITS;
   } s;
 #endif
   uint64_t v;
